4154fd9e8a95f241a5e5130a5b25b4944125b5a5
Nelson <sannelson@paypal.com>
2019-03-13 12:43:44 +0530

    config cng

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
-	"IS_APPLICATION_IN_SANDBOX" : false,
+	"IS_APPLICATION_IN_SANDBOX" : true,
 
 	"sandbox" : {
-		"CLIENT_ID" :"AUz-VyDAwn-zRsTnh4JzeGJmN82sqKjzv3-GYPEN2wDsAG82v3AWvUWCTpaJE7XGTY7ZBhjyctyuz8ZP",
-		"SECRET":"ED3HCvDcmz4KiXbm9JxVRVLLgLl--eivG5tmyJVaDj5i4sWzTneaFDWQf_rCU-Rty31JEkjH5_mzAVRQ",
+		"CLIENT_ID" :"ATc4v2WmxX53dt4Bbp5nQW0_RBPp66dJdNvWp_t5WfrtVlAQo213ln6COCz4YklplPVuDpCnY5dB_a_6",
+		"SECRET":"EPWHFE1T2Efx-tRpdI-Bzwe6nhnuQLc2W5ErOO4vBP_bmjwvhvhf8HuDMsKr-DYRa5PBkRgnxIOCZbH-",
 		"ACCESS_TOKEN_URL":"https://api.sandbox.paypal.com/v1/oauth2/token",
 		"CREATE_PAYMENT_URL":"https://api.sandbox.paypal.com/v1/payments/payment",
 		"EXECUTE_PAYMENT_URL":"https://api.sandbox.paypal.com/v1/payments/payment/{payment_id}/execute/",
@@ -14,14 +14,14 @@ var config = {
 	},
 
 	"live" : {
-		"CLIENT_ID" :"AUz-VyDAwn-zRsTnh4JzeGJmN82sqKjzv3-GYPEN2wDsAG82v3AWvUWCTpaJE7XGTY7ZBhjyctyuz8ZP",
-		"SECRET":"ED3HCvDcmz4KiXbm9JxVRVLLgLl--eivG5tmyJVaDj5i4sWzTneaFDWQf_rCU-Rty31JEkjH5_mzAVRQ",
-		"ACCESS_TOKEN_URL":"https://api.paypal.com/v1/oauth2/token",
-		"CREATE_PAYMENT_URL":"https://api.paypal.com/v1/payments/payment",
-		"EXECUTE_PAYMENT_URL":"https://api.paypal.com/v1/payments/payment/{payment_id}/execute/",
-		"GET_PAYMENT_DETAILS":"https://api.paypal.com/v1/payments/payment/{payment_id}",
-		"CANCEL_URL":"https://node-paypal-express-sever.herokuapp.com/cancel-url",
-		"RETURN_URL":"com.example.paypalcustomtabdemo://success",
+		"CLIENT_ID" :"AYBymkGzvoY4j4GlCAFt3B3lDZ0v9DPqPgLzQ-qLFDvInFseYLfY2jkDBR83V6audEq8uUHGYYPTufdV",
+		"SECRET":"EIGA-3CbWmvV5mNZQGBkbJXARbnErhE08OnbbSdq_d3WzL1_SeYwK54KQrCdMBg2yYLLpeCFy4yNUUgW",
+		"ACCESS_TOKEN_URL":"https://api.sandbox.paypal.com/v1/oauth2/token",
+		"CREATE_PAYMENT_URL":"https://api.sandbox.paypal.com/v1/payments/payment",
+		"EXECUTE_PAYMENT_URL":"https://api.sandbox.paypal.com/v1/payments/payment/{payment_id}/execute/",
+		"GET_PAYMENT_DETAILS":"https://api.sandbox.paypal.com/v1/payments/payment/{payment_id}",
+		"CANCEL_URL":"cancel-url",
+		"RETURN_URL":"cancel-url",
 		"BN_CODE":"PP-DemoPortal-EC-JSV4-python-REST"
 	}
 }
